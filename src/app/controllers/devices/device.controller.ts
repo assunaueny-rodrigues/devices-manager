5b66196652db732cd4f450e6f66a63e24d5b256c
Assunaueny Rodrigues <assu.rodrigues@attornatus.com.br>
2024-04-09 20:28:51 -0300

    feat: criando métodos no DeviceController

--- src/app/controllers/devices/device.controller.ts
+++ src/app/controllers/devices/device.controller.ts
@@ -1,10 +1,58 @@
 import { Injectable } from '@angular/core';
+import { Device } from '../../pages/devices-list/models/device.model';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeviceController {
+  // Esse serviço vai utilizar o localStorage para persistir os dados e simular uma api
 
-constructor() { }
+  readonly DEVICES_KEY = 'DEVICES_KEY'
 
+  addDevice(device: Device): Observable<Device> {
+    const currentDevicesList = this.getDevices();
+
+    currentDevicesList.push(device);
+    this.setDevices(currentDevicesList)
+
+    return of(device);
+  }
+
+
+  private getDevices(): Device[] {
+    return localStorage.getItem(this.DEVICES_KEY) ? JSON.parse(localStorage.getItem(this.DEVICES_KEY) as string) : []
+  }
+
+  private setDevices(devices: Device[]): void {
+    return localStorage.setItem(this.DEVICES_KEY, JSON.stringify(devices))
+  }
+
+  updateDevice(device: Device): Observable<Device> {
+    const currentDevicesList = this.getDevices();
+
+    const index = currentDevicesList.indexOf(device);
+    currentDevicesList[index] = device;
+
+    this.setDevices(currentDevicesList);
+
+    return of(device);
+  }
+
+  deleteDevice(device: Device): Observable<Device> {
+    const currentDevicesList = this.getDevices();
+
+    const index = currentDevicesList.indexOf(device);
+    currentDevicesList.splice(index, 1);
+
+    this.setDevices(currentDevicesList);
+
+    return of(device)
+  }
+
+  listDevices(): Observable<Device[]> {
+    const currentDevicesList = this.getDevices();
+
+    return of(currentDevicesList)
+  }
 }
